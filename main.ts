b7e02a430ad184ce946bc168b359a83080dd265d
tmheducation <68049755+tmheducation@users.noreply.github.com>
2022-10-10 11:32:32 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.showString("halöchin lissi")
+basic.showIcon(IconNames.Yes)
+basic.showString("ich bin glüklich")
+basic.showIcon(IconNames.Happy)
+basic.forever(function () {
+    music.playMelody("- A B E A C - G ", 120)
+})
